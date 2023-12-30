90dd837fc7f51238072eaebe017e5b9f4217cd3e
Raj <tiwariraj91929@gmail.com>
2023-12-30 15:31:16 +0530

    done!!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+  // 
+  if((a<0 || b<0 )||( typeof a !== 'number' || typeof b !== 'number')){
+    sum = "ERROR";
+  }else{
+   
+    if(a<b){
+    for (i=a;i<=b;i++){
+      sum += i
+     }
+    }else{
+        for (i=b;i<=a;i++){
+            sum += i}
+    }
+  }
+  
+  
+  
+    return sum
 };
 
 // Do not edit below this line
