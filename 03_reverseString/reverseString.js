50c65ce852a7e47700d26ea81d693cdac4a40899
Raj <tiwariraj91929@gmail.com>
2023-12-30 12:43:12 +0530

    done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
 
+ const reverseString = function(a) {
+    let string = "";
+    
+    for(i=a.length-1;i>=0;i--){
+    string += a[i]
+    
+    }
+        return string;
 };
-
+// hello  to olleh .... 
 // Do not edit below this line
 module.exports = reverseString;
