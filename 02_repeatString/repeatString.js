e1acf7eb97c30524c1ee2496b5b87fba12ee8c8e
Raj <tiwariraj91929@gmail.com>
2023-12-30 11:47:08 +0530

    passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(a,num){
+    if(num<0){
+        return "ERROR"}
+        else{let word =""
+    for(i=0;i<num;i++){
+        word += a
+        
+    }return word}
 };
 
 // Do not edit below this line
