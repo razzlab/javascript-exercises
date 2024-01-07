0f24c58f09b34eaaa6e5e7085fc6dbdd61d2f00c
Raj <tiwariraj91929@gmail.com>
2024-01-07 20:35:19 +0530

    done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(a) {
+    let result = [];
+    for(i=0;i<a.length;i++){
+        if(a[i].yearOfDeath-a[i].yearOfBirth>0){
+            result[i] = a[i].yearOfDeath-a[i].yearOfBirth
+        }else{result[i]=new Date().getFullYear()-a[i].yearOfBirth}
+    }
+   let index =result.indexOf(Math.max(...result));
+   return a[index]//.name
 };
 
 // Do not edit below this line
