021b03ca2d60c32e139218da49c55624e939ef7f
Raj <tiwariraj91929@gmail.com>
2024-01-07 19:17:05 +0530

    completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
+const add = function(a,b) {
+  return a+b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  return a-b
 	
 };
 
-const sum = function() {
+const sum = function(args) {
+  if(args.length==0){return 0}
+  let result = new Number(0);
+  for (i=0;i<args.length;i++){
+    result += Number(args[i])
+  }
+  return result;
 	
 };
 
-const multiply = function() {
+const multiply = function(args) {
+  let result = 1;
+  for (i=0;i<args.length;i++){
+    result = result * args[i]
+  }
+  return result;
 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  let result = 1;
+  for(i=0;i<b;i++){
+    result = result*a
+  }
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a==0){return 1}
+	let result = 1;
+  for(i=1;i<=a;i++){
+    result= result * i
+  }
+  return result
 };
 
 // Do not edit below this line
