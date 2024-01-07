b8a31e5e1ff6a3009d89a1b476bf1ce7ba8e51d0
Raj <tiwariraj91929@gmail.com>
2024-01-07 20:20:54 +0530

    done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(a) {
+let result=[];
+for(i=0;i<a.length;i++){
+    result[i]=a[i].title
+}
+return result
 };
 
 // Do not edit below this line
