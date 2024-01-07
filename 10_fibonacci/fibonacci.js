052566ba286016deb2cb8747445098db0bd7763d
Raj <tiwariraj91929@gmail.com>
2024-01-07 20:14:58 +0530

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function (a) {
+    let result = [0,1];
+    if(a<0){return "OOPS"}
+    for (i = 2; i <= a; i++) {
+        result[i] = result[i-1]+result[i-2]
+    }
+    return Number(result[a])
 
 };
 
